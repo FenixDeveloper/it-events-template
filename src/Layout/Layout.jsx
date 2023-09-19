a192a3ebe989dd9659f29ff4a44e974c2d47e7d9
Alex Martynov <fenixdeveloper@gmail.com>
2023-09-19 21:43:28 +0600

    fix: layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,13 +2,17 @@ import styles from "./styles.module.scss";
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
+import React from "@types/react";
+import {App} from "../index";
 
 export const Layout = () => {
   return (
-    <div className={styles.app}>
-      <Header />
-      <main className={styles.main}>{<Outlet />}</main>
-      <Footer />
-    </div>
+      <App>
+        <div className={styles.app}>
+          <Header />
+          <main className={styles.main}>{<Outlet />}</main>
+          <Footer />
+        </div>
+      </App>
   );
 };
